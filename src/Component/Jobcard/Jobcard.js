71fc77cebf3f3523b3f9f04417b9763254e72b7f
saivamsivarma <saivamsivarma6@gmail.com>
2021-01-28 12:36:13 +0530

    Added jobs page

---
+++ src/Component/Jobcard/Jobcard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Jobcard.css';
+
+function Jobcard(props) {
+    return (
+        <div className="card px-4 py-3">
+            <div className="d-flex justify-content-between align-items-center p-2">
+                <div className="p-1">
+                    <img src={props.img} alt="" className="img-fluid" height={props.h} width={props.h} />
+                </div>
+                <div className="">
+                    <h5>{props.role}</h5>
+                    <h6>{props.name}</h6>
+                </div>
+            </div>
+            <div className="details p-2 my-1">
+                The User Experience Designer position exists to create compelling and digital user experience through excellent design...
+            </div>
+            <div className="d-flex justify-content-between">
+            <div className="badge secondary-bg">Full time</div>
+            <div className="badge secondary-bg">Min. 1 Year</div>
+            <div className="badge secondary-bg">Senior Level</div>
+            </div>
+            <button className="btn w-full primary-btn my-3">Apply Now</button>
+        </div>
+    )
+};
+
+export default Jobcard;
--- src/Pages/User/Jobs/Jobs.js
+++ src/Pages/User/Jobs/Jobs.js
@@ -2,30 +2,36 @@ import React from 'react';
 import './Jobs.css';
 import Navbar from '../../../Component/Navbar/Navbar';
 import Search from '../../../Component/Search/Search';
+import MapContainer from '../../../Component/Map/Map';
+import Jobcard from '../../../Component/Jobcard/Jobcard';
+import Google from '../../../Assets/google.svg'
 
 function Jobs() {
     return (
         <div>
             <Navbar />
-            <div className="container">
+            <div className="container mb-2">
                 <div className="row">
-                    <div className="col-12"><Search/></div>
+                    <div className="col-12"><Search /></div>
+                    <div className="head">Jobs Near You</div>
                     <div className="col-12">
-                        <div className="card p-3"></div>
+                        <div className="card map-holder">
+                            <MapContainer />
+                        </div>
                     </div>
-                    <div className="col-4">
-                        <div className="card shadow p-3"></div>
+                    <div className="head mt-5">Based on your Skills</div>
+                    <div className="col-12 col-md-6 col-lg-4 mt-2">
+                        <Jobcard img={Google} h="50" name="Google" role="Software Developer" />
                     </div>
-                    <div className="col-4">
-                        <div className="card shadow p-3"></div>
+                    <div className="col-12 col-md-6 col-lg-4 mt-2">
+                        <Jobcard img={Google} h="50" name="Google" role="Software Developer" />
                     </div>
-                    <div className="col-4">
-                        <div className="card shadow p-3"></div>
+                    <div className="col-12 col-md-6 col-lg-4 mt-2">
+                        <Jobcard img={Google} h="50" name="Google" role="Software Developer" />
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
 
